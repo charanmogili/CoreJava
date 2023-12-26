6a05aa804bc16f2fc1135ea94f82851576488345
Sai Charan <Sai Charan@DESKTOP-FSEK0TN>
2023-12-26 08:02:30 +0530

    First Commit

---
+++ Assignment_2/src/ArrayDuplicate.java
@@ -0,0 +1,23 @@
+
+public class ArrayDuplicate {
+	public static void main(String[] args) {
+		int[] ar= {10,20,30,10,20,40,50,20};
+		int count=0;
+		for(int i=0;i<ar.length;i++)
+		{
+			for(int j=i;j<ar.length;j++)
+			{
+				if(ar[i]==ar[j] & i!=j)
+				{
+					count++;
+					//System.out.println("Duplicate element "+ar[j]);
+					ar[j]='0';
+				}
+			}
+			if(count>1 && ar[i]!='0')
+			{
+			System.out.println(ar[i]);
+			}
+		}
+	}
+}
---
+++ Assignment_2/src/SubSetArray.java
@@ -0,0 +1,29 @@
+
+public class SubSetArray {
+
+	public static void main(String[] args) {
+		int[] ar1 = {10,20,30,40,50,60};
+		int[] ar2 = {30,50,10,};
+		int flag=0;
+		
+		for(int elem1:ar2)
+		{
+			for(int elem2:ar1)
+			{
+				if(elem2==elem1 & ar2.length<ar1.length)
+				{
+					flag++;
+				}
+			}
+		}
+		if(flag!=ar2.length) 
+		{
+			System.out.println("ar2 is not a subset of ar1");
+		}
+		else
+		{
+			System.out.println("ar2 is a subset of ar1");
+		}
+	}
+
+}
